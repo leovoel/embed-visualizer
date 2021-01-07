a43b9454a3c66945330c58a071559f49273f8cc2
Liz3 <accs@liz3.net>
2021-01-07 07:46:05 +0100

    update

--- src/components/codemodal.jsx
+++ src/components/codemodal.jsx
@@ -1,40 +1,41 @@
-import React from 'react';
-import hljs from 'highlight.js';
-import Modal from './modal';
+import React from "react";
+import hljs from "highlight.js";
+import Modal from "./modal";
 
-import discord4j from '../snippets/discord4j';
-import discordnet from '../snippets/discordnet';
-import dsharpplus from '../snippets/dsharpplus';
-import dsharpplusEmbedbuilder from '../snippets/dsharpplus-embedbuilder';
-import discordpy from '../snippets/discordpy';
-import discordie from '../snippets/discordie';
-import discordjs from '../snippets/discordjs';
-import discordio from '../snippets/discordio';
-import restcord from '../snippets/restcord';
-import eris from '../snippets/eris';
-import discordrb from '../snippets/discordrb';
-import jda from '../snippets/jda';
-
+import discord4j from "../snippets/discord4j";
+import discordnet from "../snippets/discordnet";
+import dsharpplus from "../snippets/dsharpplus";
+import dsharpplusEmbedbuilder from "../snippets/dsharpplus-embedbuilder";
+import discordpy from "../snippets/discordpy";
+import discordie from "../snippets/discordie";
+import discordjs from "../snippets/discordjs";
+import discordio from "../snippets/discordio";
+import restcord from "../snippets/restcord";
+import eris from "../snippets/eris";
+import discordrb from "../snippets/discordrb";
+import jda from "../snippets/jda";
+import discordjsv12 from "../snippets/discordjsv12";
 
 const libraries = {
-  'dotnet_discord-net': discordnet,
-  'dotnet_dsharpplus': dsharpplus,
-  'dotnet_dsharpplusEmbedbuilder': dsharpplusEmbedbuilder,
-  'python_discord-py': discordpy,
-  'js_discordie': discordie,
-  'js_discordjs': discordjs,
-  'js_discordio': discordio,
-  'js_eris': eris,
-  'php_restcord': restcord,
-  'ruby_discordrb': discordrb,
-  'java_discord4j': discord4j,
-  'java_jda': jda,
+  "dotnet_discord-net": discordnet,
+  dotnet_dsharpplus: dsharpplus,
+  dotnet_dsharpplusEmbedbuilder: dsharpplusEmbedbuilder,
+  "python_discord-py": discordpy,
+  js_discordie: discordie,
+  js_discordjs: discordjs,
+  js_discordio: discordio,
+  js_eris: eris,
+  js_discordjsv12: discordjsv12,
+  php_restcord: restcord,
+  ruby_discordrb: discordrb,
+  java_discord4j: discord4j,
+  java_jda: jda,
 };
 
 // TODO: check for localStorage availability?
 // are we ever going to run into a browser that supports flexbox but not localStorage?
 
-const LOCAL_STORAGE_KEY = 'codegen_lib';
+const LOCAL_STORAGE_KEY = "codegen_lib";
 
 const CodeModal = React.createClass({
   getInitialState() {
@@ -52,55 +53,62 @@ const CodeModal = React.createClass({
   },
 
   changeLibrary(event) {
-    localStorage.setItem('codegen_lang', event.target.value);
+    localStorage.setItem("codegen_lang", event.target.value);
     this.setState({ library: event.target.value });
   },
 
   render() {
     const { data, hasError, webhookMode, ...props } = this.props;
-    let code = 'Errors encountered when validating/parsing your data.\nCheck those first before trying to generate code.';
-    let language = 'accesslog';
+    let code =
+      "Errors encountered when validating/parsing your data.\nCheck those first before trying to generate code.";
+    let language = "accesslog";
 
-    if (webhookMode) {
-      // TODO: add support for this in whatever libraries support it directly?
-      // seems like very few of them do it
-      code = 'Webhook mode not supported yet.';
-    } else if (!hasError) {
-      language = libraries[this.state.library].language;
-      code = libraries[this.state.library].generateFrom(data);
+    if (!hasError) {
+      const lib = libraries[this.state.library];
+      if (webhookMode && !lib.webhook_support) {
+        code = `${lib.name}, does not have webhook support yet!`;
+      } else {
+        language = lib.language;
+        code = lib.generateFrom(data, webhookMode);
+      }
     }
 
-    const theme = `atom-one-${this.props.darkTheme ? 'dark' : 'light'}`;
+    const theme = `atom-one-${this.props.darkTheme ? "dark" : "light"}`;
     const highlightedBlock = hljs.highlight(language, code, true);
 
     return (
-      <Modal title='Generate code' {...props} maxWidth='90ch'>
-        <div className='ma3'>
-
-          <div className='mv2 flex flex-auto flex-column'>
-            <div className='bg-dark-red washed-blue pa2 mb2'>
-              <strong>NOTE:</strong> These code snippets may need changes to work in your
-              actual program, and they may not even be correct. Do <strong>NOT</strong> just copy and paste them in
-              without understanding what they mean.
+      <Modal title="Generate code" {...props} maxWidth="90ch">
+        <div className="ma3">
+          <div className="mv2 flex flex-auto flex-column">
+            <div className="bg-dark-red washed-blue pa2 mb2">
+              <strong>NOTE:</strong> These code snippets may need changes to
+              work in your actual program, and they may not even be correct. Do{" "}
+              <strong>NOT</strong> just copy and paste them in without
+              understanding what they mean.
             </div>
             <select
-              className='w-100 h2 mb2'
+              className="w-100 h2 mb2"
               value={this.state.library}
               onChange={this.changeLibrary}
             >
-              {Object.keys(libraries).sort().map(k => {
-                return <option value={k} key={k}>{libraries[k].name}</option>;
-              })}
+              {Object.keys(libraries)
+                .sort()
+                .map((k) => {
+                  return (
+                    <option value={k} key={k}>
+                      {libraries[k].name}
+                    </option>
+                  );
+                })}
             </select>
 
-            <pre className='ma0'>
+            <pre className="ma0">
               <code
                 className={`${theme} hljs ${highlightedBlock.language}`}
                 dangerouslySetInnerHTML={{ __html: highlightedBlock.value }}
               />
             </pre>
           </div>
-
         </div>
       </Modal>
     );
@@ -110,6 +118,5 @@ const CodeModal = React.createClass({
 function wrapper(props) {
   return <CodeModal {...props} />;
 }
-
 
 export default wrapper;
--- src/snippets/discordpy.js
+++ src/snippets/discordpy.js
@@ -1,14 +1,15 @@
-import Moment from 'moment';
-
+import Moment from "moment";
 
 function pythonKwargs(args) {
-  return Object.keys(args).reduce((kwargs, key) => {
-    if (args[key] !== null) {
-      return kwargs.concat(`${key}=${args[key]}`);
-    }
+  return Object.keys(args)
+    .reduce((kwargs, key) => {
+      if (args[key] !== null) {
+        return kwargs.concat(`${key}=${args[key]}`);
+      }
 
-    return kwargs;
-  }, []).join(', ');
+      return kwargs;
+    }, [])
+    .join(", ");
 }
 
 function toTitleCase(s) {
@@ -26,21 +27,34 @@ export default {
 
     if (data.embed) {
       // TODO: don't duplicate this kind of parsing
-      let timestamp = Moment(data.embed.timestamp !== undefined ? data.embed.timestamp : null);
+      let timestamp = Moment(
+        data.embed.timestamp !== undefined ? data.embed.timestamp : null
+      );
       timestamp = timestamp.isValid() ? timestamp.unix() : null;
 
       const args = {
         title: data.embed.title ? JSON.stringify(data.embed.title) : null,
-        colour: data.embed.color ? `discord.Colour(0x${data.embed.color.toString(16)})` : null,
+        colour: data.embed.color
+          ? `discord.Colour(0x${data.embed.color.toString(16)})`
+          : null,
         url: data.embed.url ? JSON.stringify(data.embed.url) : null,
-        description: data.embed.description ? JSON.stringify(data.embed.description) : null,
-        timestamp: timestamp ? `datetime.datetime.utcfromtimestamp(${timestamp})` : null
+        description: data.embed.description
+          ? JSON.stringify(data.embed.description)
+          : null,
+        timestamp: timestamp
+          ? `datetime.datetime.utcfromtimestamp(${timestamp})`
+          : null,
       };
 
       result.push(`embed = discord.Embed(${pythonKwargs(args)})`);
 
-      if (data.embed.image || data.embed.thumbnail || data.embed.author || data.embed.footer) {
-        result.push('');
+      if (
+        data.embed.image ||
+        data.embed.thumbnail ||
+        data.embed.author ||
+        data.embed.footer
+      ) {
+        result.push("");
       }
 
       if (data.embed.image) {
@@ -51,7 +65,9 @@ export default {
 
       if (data.embed.thumbnail) {
         const thumbnail = data.embed.thumbnail;
-        const args = { url: thumbnail.url ? JSON.stringify(thumbnail.url) : null };
+        const args = {
+          url: thumbnail.url ? JSON.stringify(thumbnail.url) : null,
+        };
         result.push(`embed.set_thumbnail(${pythonKwargs(args)})`);
       }
 
@@ -60,43 +76,50 @@ export default {
         const args = {
           name: author.name ? JSON.stringify(author.name) : null,
           url: author.url ? JSON.stringify(author.url) : null,
-          icon_url: author.icon_url ? JSON.stringify(author.icon_url) : null
+          icon_url: author.icon_url ? JSON.stringify(author.icon_url) : null,
         };
         result.push(`embed.set_author(${pythonKwargs(args)})`);
       }
 
       if (data.embed.footer) {
         const args = {
-          text: data.embed.footer.text ? JSON.stringify(data.embed.footer.text) : null,
-          icon_url: data.embed.footer.icon_url ? JSON.stringify(data.embed.footer.icon_url) : null
+          text: data.embed.footer.text
+            ? JSON.stringify(data.embed.footer.text)
+            : null,
+          icon_url: data.embed.footer.icon_url
+            ? JSON.stringify(data.embed.footer.icon_url)
+            : null,
         };
 
         result.push(`embed.set_footer(${pythonKwargs(args)})`);
       }
 
       if (data.embed.fields) {
-        result.push('');
+        result.push("");
         for (const field of data.embed.fields) {
           const args = {
             name: field.name ? JSON.stringify(field.name) : null,
             value: field.value ? JSON.stringify(field.value) : null,
-            inline: field.inline !== undefined ? toTitleCase(field.inline.toString()) : null
+            inline:
+              field.inline !== undefined
+                ? toTitleCase(field.inline.toString())
+                : null,
           };
           result.push(`embed.add_field(${pythonKwargs(args)})`);
         }
       }
 
-      result.push('');
+      result.push("");
     }
 
     if (data.content || data.embed) {
       const args = {
         content: data.content ? JSON.stringify(data.content) : null,
-        embed: data.embed ? 'embed' : null
+        embed: data.embed ? "embed" : null,
       };
-      result.push(`await bot.say(${pythonKwargs(args)})`);
+      result.push(`await ctx.send(${pythonKwargs(args)})`);
     }
 
-    return result.join('\n');
-  }
+    return result.join("\n");
+  },
 };
